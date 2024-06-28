8981fb399495381f5a409688cf9f4b6954f1fdcc
anyou4432 <kyoandya@gmail.com>
2024-06-28 23:28:17 +0900

    adduser_post

--- src/users/AddUser.tsx
+++ src/users/AddUser.tsx
@@ -1,7 +1,84 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 
 export default function AddUser() {
+
+  let navigate = useNavigate()
+
+  type User = {
+    name: string
+    username: string
+    email: string
+  }
+  const [user, setUser] = useState<User>({
+    name: "",
+    username: "",
+    email: "",
+  })
+
+  const {name, username, email} = user
+
+  const onInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUser({...user, [e.target.name]: e.target.value})
+  }
+
+  const onSubmit = async(e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    await axios.post("http://localhost:8080/user", user)
+    navigate("/")
+  }
+
   return (
-    <div>AddUser Form</div>
+    <div className="container">
+      <div className="row">
+        <div className="col-md-5 offset-md-3 border rounded p-4 mt-2 shadow">
+          <h2 className="text-enter m-4">Register User</h2>
+          <form onSubmit={(e) => onSubmit(e)}>
+            <div className="mb-3">
+              <label htmlFor="Name" className="form-lael">
+                Name
+              </label>
+              <input
+                type={"text"}
+                className="form-control"
+                placeholder="Enter your name"
+                name="name"
+                value={name}
+                onChange={(e) => onInputChange(e)}
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="Name" className="form-lael">
+                Username
+              </label>
+              <input
+                type={"text"}
+                className="form-control"
+                placeholder="Enter your username"
+                name="username"
+                value={username}
+                onChange={(e) => onInputChange(e)}
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="Name" className="form-lael">
+                Email
+              </label>
+              <input
+                type={"text"}
+                className="form-control"
+                placeholder="Enter your email address"
+                name="email"
+                value={email}
+                onChange={(e) => onInputChange(e)}
+              />
+            </div>
+            <button type="submit" className="btn btn-outline-primary">Submit</button>
+            <Link className="btn btn-outline-danger mx-2" to="/">Cancel</Link>
+          </form>
+        </div>
+      </div>
+    </div>
   )
 }
